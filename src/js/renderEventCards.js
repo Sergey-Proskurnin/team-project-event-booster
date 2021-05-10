100375cc087f2e53d7cb4fcd70097ebab023f925
Sergey-Proskurnin <proskurnin@ua.fm>
2021-05-11 02:19:23 +0300

    Fixed pagination and adds values on fetch

--- src/js/renderEventCards.js
+++ src/js/renderEventCards.js
@@ -1,43 +1,38 @@
-// import NewApiService from './apiService';
-// import eventsCardTmpl from '../templates/eventsCardTmpl.hbs';
+// // import NewApiService from './apiService';
+// // import eventsCardTmpl from '../templates/eventsCardTmpl.hbs';
 
 
-const apiService = new NewApiService();
-const inputSearch = document.querySelector('.form-submit');
-const eventCardsRef = document.querySelector('.event-cards');
-export let resultGallery = [];
-// console.dir(inputSearch);
-inputSearch.addEventListener('submit', onSearch);
+// const apiService = new NewApiService();
+// const inputSearch = document.querySelector('.form-submit');
+// const eventCardsRef = document.querySelector('.event-cards');
+// export let resultGallery = [];
+// // console.dir(inputSearch);
+// inputSearch.addEventListener('submit', onSearch);
 
-async function onSearch(e) {
-  e.preventDefault();
-  // console.log(e.target.elements);
-  eventCardsRef.innerHTML = '';
-  const valueInput = e.target.elements[0].value;
-  const valueSelect = e.target.nextElementSibling[0].value;
-  console.dir(e.target);
+// async function onSearch(e) {
+//   e.preventDefault();
+//   // console.log(e.target.elements);
+//   eventCardsRef.innerHTML = '';
+//   const valueInput = e.target.elements[0].value;
+//   const valueSelect = e.target.nextElementSibling[0].value;
+//   console.dir(e.target);
 
-//   apiService.query = [valueInput, valueSelect];
-//   console.log(apiService.query);
+// //   apiService.query = [valueInput, valueSelect];
+// //   console.log(apiService.query);
 
 
 
-  const galleryArray = await apiService.fetchApi();
-  resultGallery.push(...galleryArray);
-
-  const markup = await appendEventMarkup(galleryArray);
-  return markup;
-}
-
+//   const galleryArray = await apiService.fetchApi();
+//   resultGallery.push(...galleryArray);
 
 //   const markup = await appendEventMarkup(galleryArray);
 //   return markup;
 // }
 
 
-function appendEventMarkup(events) {
-  eventCardsRef.insertAdjacentHTML('beforeend', eventsCardTmpl(events));
-}
+// //   const markup = await appendEventMarkup(galleryArray);
+// //   return markup;
+// // }
 
 
 // function appendEventMarkup(events) {
@@ -45,6 +40,11 @@ function appendEventMarkup(events) {
 // }
 
 
-// export { resultGallery }
-// console.log(resultGallery);
+// // function appendEventMarkup(events) {
+// //   eventCardsRef.insertAdjacentHTML('beforeend', eventsCardTmpl(events));
+// // }
+
+
+// // export { resultGallery }
+// // console.log(resultGallery);
 
