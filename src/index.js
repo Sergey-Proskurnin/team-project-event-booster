95e1ede4b11ac2e7a21abd40541f3cd8e8fdc150
Zharkynbek Idirisov <zh.idirisov@gmail.com>
2021-05-10 23:30:34 +0200

    add pagination

--- src/index.js
+++ src/index.js
@@ -2,10 +2,58 @@
 // import './sass/utils/variables.scss'
 import 'material-design-icons/iconfont/material-icons.css';
 import './sass/main.scss';
-import './js/apiService';
+import './sass/components/pagination.scss';
+// import './js/apiService';
 import './js/renderEventCards';
 import './js/renderOptionSelect';
-import './js/test'
+import './js/test';
+// import apiService from './js/apiService';
+import pagination from 'paginationjs';
+import eventsCardTmpl from './templates/eventsCardTmpl.hbs';
 // import { resultGallery } from './js/test'
 // console.log(resultGallery);
 
+const refs = {
+  searchForm: document.querySelector('.search-form'),
+  searchInput: document.querySelector('.input'),
+  select: document.querySelector('.select'),
+};
+
+let fetchResult = [];
+export { fetchResult };
+
+// countryCode = ${refs.select.value}
+
+class ApiService {
+  static getData() {
+    $('#demo').pagination({
+      dataSource: function (done) {
+        $.ajax({
+          type: 'GET',
+          url: `https://app.ticketmaster.com/discovery/v2/events.json?keyword=${refs.searchInput.value}&countryCode=${refs.select.value}&apikey=k4ZuaibW7VaW2DqWiJtNRmwq3dAdRpv6`,
+          success: function (data) {
+            console.log(data);
+            if ('_embedded' in data) {
+              done(data._embedded.events);
+              fetchResult.push(...data._embedded.events);
+            }
+          },
+        });
+      },
+      pageSize: 10,
+      showPrevious: false,
+      showNext: false,
+      callback: function (data) {
+        console.log(data);
+        $('#dataContainer').html(eventsCardTmpl(data));
+      },
+    });
+  }
+}
+
+function onSubmitForm(e) {
+  e.preventDefault();
+  ApiService.getData();
+}
+
+refs.searchForm.addEventListener('submit', onSubmitForm);
--- src/js/renderEventCards.js
+++ src/js/renderEventCards.js
@@ -1,38 +1,38 @@
-import NewApiService from './apiService';
-import eventsCardTmpl from '../templates/eventsCardTmpl.hbs';
+// import NewApiService from './apiService';
+// import eventsCardTmpl from '../templates/eventsCardTmpl.hbs';
 
-const apiService = new NewApiService();
-const inputSearch = document.querySelector('.form-submit');
-const eventCardsRef = document.querySelector('.event-cards');
-export let resultGallery = []
+// const apiService = new NewApiService();
+// const inputSearch = document.querySelector('.form-submit');
+// const eventCardsRef = document.querySelector('.event-cards');
+// export let resultGallery = [];
 
-inputSearch.addEventListener('submit', onSearch);
+// inputSearch.addEventListener('submit', onSearch);
 
-async function onSearch(e) {
-  e.preventDefault();
-  eventCardsRef.innerHTML = ''
-  const valueInput =e.target.elements[0].value;
-  const valueSelect =e.target.elements[2].value;
+// async function onSearch(e) {
+//   e.preventDefault();
+//   eventCardsRef.innerHTML = '';
+//   const valueInput = e.target.elements[0].value;
+//   const valueSelect = e.target.elements[2].value;
 
-  apiService.query = [valueInput, valueSelect];
-  console.log(apiService.query);
+//   apiService.query = [valueInput, valueSelect];
+//   console.log(apiService.query);
 
-  const galleryArray = await apiService.fetchApi();
-  resultGallery.push(...galleryArray)
-  
-  const markup = await appendEventMarkup(galleryArray)
-  return markup;
-}
+//   const galleryArray = await apiService.fetchApi();
+//   resultGallery.push(...galleryArray);
 
-function renderEventCards() {
-  return apiService.fetchApi().then(appendEventMarkup);
-}
+//   const markup = await appendEventMarkup(galleryArray);
+//   return markup;
+// }
 
-function appendEventMarkup(events) {
-  eventCardsRef.insertAdjacentHTML('beforeend', eventsCardTmpl(events));
-  }
+// function renderEventCards() {
+//   return apiService.fetchApi().then(appendEventMarkup);
+// }
 
-renderEventCards();
+// function appendEventMarkup(events) {
+//   eventCardsRef.insertAdjacentHTML('beforeend', eventsCardTmpl(events));
+// }
 
-// export { resultGallery }
-// console.log(resultGallery);
+// renderEventCards();
+
+// // export { resultGallery }
+// // console.log(resultGallery);
