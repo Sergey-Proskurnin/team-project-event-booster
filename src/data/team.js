3fa76bb0652c596121a48447a8805f2e61cf46fe
Alla Karhopolova <alalasimys@gmail.com>
2021-05-14 18:36:33 +0300

    add team photo

--- src/data/team.js
+++ src/data/team.js
@@ -1,16 +1,14 @@
 export default [
   {
     fullName: 'Olena Kaminska',
-    photo:
-      'https://files.slack.com/files-pri/T01J5GWLAAC-F021U5THSPP/img_1674_1.jpg',
+    photo: 'src/images/team/Kaminska_Olena.jpeg',
     linkedIn: 'https://www.linkedin.com/in/helen-kaminskaya-b1188351/',
     gitHub: 'https://github.com/OlenaKa',
     instagram: 'https://www.instagram.com/helenkaminskaya/',
   },
   {
-    fullName: 'Idirisov Zharkynbek',
-    photo:
-      'https://files.slack.com/files-pri/T01J5GWLAAC-F0229TL94GZ/screenshot_20200814-135450_instagram.jpg',
+    fullName: 'Zharkynbek Idirisov',
+    photo: 'src/images/team/Zharkynbek_Idirisov.jpeg',
     linkedIn:
       'https://www.linkedin.com/mwlite/in/%E2%9A%A1zharkynbek-idirisov-%E2%9A%A1-b41578206',
     gitHub: 'https://github.com/Zharkynbek',
@@ -18,39 +16,40 @@ export default [
   },
   {
     fullName: 'Irina Vislohuzova',
-    photo:
-      'https://files.slack.com/files-pri/T01J5GWLAAC-F021UJVR1UM/image_from_ios.jpg',
-    linkedIn: '',
-    gitHub: '',
+    photo: 'src/images/team/Vislohuzova_Irina.jpeg',
+    linkedIn: '#',
+    gitHub: 'https://github.com/irina-visloguzova',
     instagram: 'https://instagram.com/v.irinaa?igshid=v6xbfhcnm2pr',
   },
   {
-    fullName: '',
-    photo: '',
-    linkedIn: '',
-    gitHub: '',
-    instagram: '',
+    fullName: 'Sergey Proskurnin',
+    photo:
+      'https://cdn22.img.ria.ru/images/153307/00/1533070025_320:0:1599:1279_1920x0_80_0_0_3290f87650d42f21292cefd0a509b811.jpg',
+    linkedIn: '#',
+    gitHub: '#',
+    instagram: '#',
   },
   {
-    fullName: '',
-    photo: '',
-    linkedIn: '',
-    gitHub: '',
-    instagram: '',
+    fullName: 'Dmitryi Sagaidachnyi',
+    photo: 'src/images/team/Sagaidachny_Dmitry.jpeg',
+    linkedIn:
+      'https://www.linkedin.com/in/%D0%B4%D0%BC%D0%B8%D1%82%D1%80%D0%B8%D0%B9-%D1%81%D0%B0%D0%B3%D0%B0%D0%B9%D0%B4%D0%B0%D1%87%D0%BD%D1%8B%D0%B9-3052b3203',
+    gitHub: 'https://github.com/Dim4ik85',
+    instagram: '#',
   },
   {
-    fullName: '',
-    photo: '',
-    linkedIn: '',
-    gitHub: '',
-    instagram: '',
+    fullName: 'Ekaterina Sivak',
+    photo: 'src/images/team/Sivak_Ekaterina.jpeg',
+    linkedIn: 'https://www.linkedin.com/feed/',
+    gitHub: 'https://github.com/SivakKaterina',
+    instagram: 'https://www.instagram.com/ksivak335/',
   },
   {
-    fullName: '',
+    fullName: 'Sergii Maksymenko',
     photo: '',
-    linkedIn: '',
-    gitHub: '',
-    instagram: '',
+    linkedIn: 'https://www.linkedin.com/in/sergiimaksymenko/',
+    gitHub: 'https://github.com/uaMonterey',
+    instagram: 'https://www.instagram.com/s.v.maksymenko/',
   },
   {
     fullName: '',
