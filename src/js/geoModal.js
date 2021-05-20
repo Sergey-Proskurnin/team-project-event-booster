6da6c6b8db6f07d38b4feb3c4529009a12a589e9
Sergey-Proskurnin <proskurnin@ua.fm>
2021-05-21 02:26:24 +0300

    Big markup fix and comment cleanup

--- src/js/geoModal.js
+++ src/js/geoModal.js
@@ -7,36 +7,31 @@ eventCardRef.addEventListener('click', onCardClick);
 
 function onCardClick(e) {
   e.preventDefault();
-  let fetchResult = JSON.parse(localStorage.getItem('data'));
-  if (
-    e.target.classList.contains('event-place')) {
-      const id = e.target.parentNode.id;
-      const evtInfo = fetchResult.find(evt => evt.id === id);
-      const evtInfoMarkup = geoTmp(evtInfo);
-        openModal(evtInfoMarkup);
-        console.log(evtInfoMarkup);
-    }}
-    
+  if (e.target.nodeName === 'A') {
+    const urlMap = e.target.attributes[0].value;
+    const markupCard = geoTmp(urlMap);
+    openModal(markupCard);
+  }
+  return;
+  }
 
-    function openModal(markupInfo) {
-    const geoModal = basicLightbox.create(`${markupInfo}`, {
-      onShow: geoModal => {
-        document.body.style.overflow = 'hidden';
-        document.addEventListener('keyup', closeOnEsc);
-      },
-      onClose: geoModal => {
-        document.body.style.overflow = 'auto';
-        document.removeEventListener('keyup', closeOnEsc);
-      },
-    }); 
-    geoModal.show();
+  
+function openModal(markupInfo) {
+  const geoModal = basicLightbox.create(`${markupInfo}`, {
+    onShow: geoModal => {
+      document.body.style.overflow = 'hidden';
+      document.addEventListener('keyup', closeOnEsc);
+    },
+    onClose: geoModal => {
+      document.body.style.overflow = 'auto';
+      document.removeEventListener('keyup', closeOnEsc);
+    },
+  });
+  geoModal.show();
 
-    function closeOnEsc(e) {
-      if (e.key === 'Escape') {
-        geoModal.close();
-      }
-  }};
-
-
-
-    
+  function closeOnEsc(e) {
+    if (e.key === 'Escape') {
+      geoModal.close();
+    }
+  }
+}
